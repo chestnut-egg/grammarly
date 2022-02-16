38b6b103fb4c4c6dc95fc88807253b4051d28137
chestnut-egg <1048232111@qq.com>
2022-02-16 22:53:05 +0800

    add uploader

--- control.py
+++ control.py
@@ -1,6 +1,9 @@
 from flask import *
+import werkzeug
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'upload/'
 
 @app.route('/index')
 def index():
@@ -8,6 +11,12 @@ def index():
     info['name'] = 'name'
     return render_template('index.html', info = info)
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == '__main__':
     app.debug = True
